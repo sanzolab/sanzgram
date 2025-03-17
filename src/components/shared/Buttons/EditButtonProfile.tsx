9fb017ce23f51bcb8e1e8c68da0c1ee6883a1cb5
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-17 15:01:46 -0500

    feat: refactor EditButtonProfile component to use Link for navigation and improve code structure

--- src/components/shared/Buttons/EditButtonProfile.tsx
+++ src/components/shared/Buttons/EditButtonProfile.tsx
@@ -1,23 +1,22 @@
-import { Button } from '@/components/ui/button'
+import DinamicEditIcon from "/public/assets/icons/dinamicEditIcon";
+import { Link } from "react-router-dom";
 
-import DinamicEditIcon from '/public/assets/icons/dinamicEditIcon'
-const EditButtonProfile = () => {
+type EditButtonProps = {
+  userId: string;
+};
+const EditButtonProfile = ({ userId }: EditButtonProps) => {
+  return (
+    <Link
+      to={`/update-profile/${userId}`}
+      className="shad-button_dark_no_h h  group"
+      aria-label="Boton editar perfil"
+    >
+      {" "}
+      {<DinamicEditIcon color="#FFB620" height={16} width={16} />}
+      {/* <img src='/public/assets/icons/edit.svg' alt="Edit icon" className='fill-slate-300' height={16} width={16} /> */}
+      <p className="subtle-semibold">Edit Profile</p>
+    </Link>
+  );
+};
 
-    return (
-        <Button
-            className='shad-button_dark_no_h  group'
-            aria-label="Boton editar perfil"
-            onClick={() => {
-
-            }}
-        >   {
-                <DinamicEditIcon color='#FFB620' height={16} width={16} />
-            }
-            {/* <img src='/public/assets/icons/edit.svg' alt="Edit icon" className='fill-slate-300' height={16} width={16} /> */}
-            <p className='subtle-semibold'>Edit Profile</p>
-
-        </Button>
-    )
-}
-
-export default EditButtonProfile
+export default EditButtonProfile;
