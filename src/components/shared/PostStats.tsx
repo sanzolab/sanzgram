8ae926d1132bd3f2f6f80bb958de7c84deea0634
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-17 15:01:42 -0500

    feat: refactor PostStats component for improved code structure and readability

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,91 +1,102 @@
-import { useDeleteSavedPost, useGetCurrentUser, useLikedPost, useSavePost } from '@/lib/react-query/queriesAndMutations'
-import { checkIsLiked } from '@/lib/utils'
-import { Models } from 'appwrite'
-import React, { useEffect, useState } from 'react'
-import Loader from './Loader'
+import {
+  useDeleteSavedPost,
+  useGetCurrentUser,
+  useLikedPost,
+  useSavePost,
+} from "@/lib/react-query/queriesAndMutations";
+import { checkIsLiked } from "@/lib/utils";
+import { Models } from "appwrite";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
 
 type PostStatsprops = {
-    post?: Models.Document
-    userId: string
-}
+  post?: Models.Document;
+  userId: string;
+};
 const PostStats = ({ post, userId }: PostStatsprops) => {
+  const likesList = post?.likes.map((user: Models.Document) => user.$id);
 
-    const likesList = post?.likes.map((user: Models.Document) => user.$id)
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
 
-    const [likes, setLikes] = useState(likesList)
-    const [isSaved, setIsSaved] = useState(false)
+  const { mutate: likePost } = useLikedPost();
+  const { mutate: savePost, isPending: isSavingPost } = useSavePost();
+  const { mutate: deleteSavepost, isPending: isDelitingSaved } =
+    useDeleteSavedPost();
 
+  const { data: currentuser } = useGetCurrentUser();
+  const savedPostRecord = currentuser?.save.find(
+    (record: Models.Document) => record.post.$id === post?.$id
+  );
 
-    const { mutate: likePost } = useLikedPost()
-    const { mutate: savePost, isPending: isSavingPost } = useSavePost()
-    const { mutate: deleteSavepost, isPending: isDelitingSaved } = useDeleteSavedPost()
+  useEffect(() => {
+    setIsSaved(!!savedPostRecord);
+  }, [currentuser]);
 
-    const { data: currentuser } = useGetCurrentUser()
-    const savedPostRecord = currentuser?.save.find((record: Models.Document) => record.post.$id === post?.$id)
+  const handleLikePost = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    let newLikes = [...likes];
 
-    useEffect(() => {
-        setIsSaved(!!savedPostRecord)
-    }, [currentuser])
-
-    const handleLikePost = (e: React.MouseEvent) => {
-        e.stopPropagation()
-        let newLikes = [...likes]
-
-        const hasLiked = newLikes.includes(userId)
-        if (hasLiked) {
-            newLikes = newLikes.filter(id => id !== userId)
-        } else {
-            newLikes.push(userId)
-        }
-        setLikes(newLikes)
-        likePost({ postId: post?.$id || '', likesArray: newLikes })
+    const hasLiked = newLikes.includes(userId);
+    if (hasLiked) {
+      newLikes = newLikes.filter((id) => id !== userId);
+    } else {
+      newLikes.push(userId);
     }
-    const handleSavePost = (e: React.MouseEvent) => {
-        e.stopPropagation()
+    setLikes(newLikes);
+    likePost({ postId: post?.$id || "", likesArray: newLikes });
+  };
+  const handleSavePost = (e: React.MouseEvent) => {
+    e.stopPropagation();
 
-        if (savedPostRecord || userId === '') {
-            setIsSaved(false)
-            deleteSavepost(savedPostRecord.$id)
-            return
-        }
-
-        savePost({ postId: post?.$id || '', userId })
-        setIsSaved(true)
-
+    if (savedPostRecord || userId === "") {
+      setIsSaved(false);
+      deleteSavepost(savedPostRecord.$id);
+      return;
     }
-    return (
-        <div className='flex justify-between  z-20'>
-            <div className='flex gap-2 mr-5'>
-                <img
-                    src={`${checkIsLiked(likes, userId)
-                        ? '/public/assets/icons/liked.svg'
-                        : '/public/assets/icons/like.svg'}
+
+    savePost({ postId: post?.$id || "", userId });
+    setIsSaved(true);
+  };
+  return (
+    <div className="flex justify-between  z-20">
+      <div className="flex gap-2 mr-5">
+        <img
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "/public/assets/icons/liked.svg"
+              : "/public/assets/icons/like.svg"
+          }
                         `}
-                    alt='like button'
-                    width={20}
-                    height={20}
-                    onClick={handleLikePost}
-                    className='cursor-pointer'
-                />
+          alt="like button"
+          width={20}
+          height={20}
+          onClick={handleLikePost}
+          className="cursor-pointer"
+        />
 
-                <p className='small-medium lg:base-medium'>{likes.length}</p>
-            </div>
-            <div className='flex gap-2 mr-5'>
-                {
-                    isSavingPost || isDelitingSaved
-                        ? <Loader />
-                        : <img
-                            src={`${isSaved ? '/public/assets/icons/saved.svg' : '/public/assets/icons/save.svg'}`}
-                            alt='like button'
-                            width={20}
-                            height={20}
-                            onClick={handleSavePost}
-                            className='cursor-pointer'
-                        />
-                }
-            </div>
-        </div>
-    )
-}
+        <p className="small-medium lg:base-medium">{likes.length}</p>
+      </div>
+      <div className="flex gap-2 mr-5">
+        {isSavingPost || isDelitingSaved ? (
+          <Loader />
+        ) : (
+          <img
+            src={`${
+              isSaved
+                ? "/public/assets/icons/saved.svg"
+                : "/public/assets/icons/save.svg"
+            }`}
+            alt="like button"
+            width={20}
+            height={20}
+            onClick={handleSavePost}
+            className="cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default PostStats
+export default PostStats;
