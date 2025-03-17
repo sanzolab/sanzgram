3c73cbb2caf61616ce9b4309b17c02e6375b2767
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-17 13:14:54 -0500

    refactor: standardize quotes and formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,49 @@
-import { Routes, Route } from 'react-router-dom'
-import './globals.css'
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
 
-import SignInForm from './_auth/forms/SignInForm'
-import SignUpForm from './_auth/forms/SignUpForm'
-import AuthLayout from './_auth/AuthLayout'
-import RootLayout from './_root/RootLayout'
-import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages'
-import { Toaster } from "@/components/ui/toaster"
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
-    return (
-        <main className='flex flex-column max-h-screen h-screen'>
-            <Routes>
-                {/* Public */}
-                <Route element={<AuthLayout />}>
-                    <Route path='/sign-in' element={<SignInForm />} />
-                    <Route path='/sign-up' element={<SignUpForm />} />
-                </Route>
+  return (
+    <main className="flex flex-column max-h-screen h-screen">
+      <Routes>
+        {/* Public */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
 
-                {/* Private */}
-                <Route element={<RootLayout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/explore' element={<Explore />} />
-                    <Route path='/saved' element={<Saved />} />
-                    <Route path='/all-users' element={<AllUsers />} />
-                    <Route path='/create-post' element={<CreatePost />} />
-                    <Route path='/update-post/:id' element={<EditPost />} />
-                    <Route path='/profile/:id/*' element={<Profile />} />
-                    <Route path='/update-profile/:id' element={<UpdateProfile />} />
-                    <Route path='/post/:id' element={<PostDetails />} />
-
-
-                </Route>
-
-            </Routes>
-            <Toaster />
-        </main>
-    )
+        {/* Private */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </main>
+  );
 }
 
-export default App
+export default App;
