0bd47966f6277b1691d3a291e66d11b3c085955b
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-17 15:01:04 -0500

    feat: enhance LikedPosts component to display liked posts in a grid format

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import GridPostList from "@/components/shared/GridPostList";
+import { Models } from "appwrite";
 
-const LikedPosts = () => {
-    return (
-        <div>LikedPosts</div>
-    )
-}
+type LikedPostsProps = {
+  likedPosts: Models.Document;
+  username: string;
+};
+const LikedPosts = ({ likedPosts, username }: LikedPostsProps) => {
+  return (
+    <div className="w-full mt-12 relative">
+      <ul className="grid grid-cols-auto-fit">
+        <GridPostList
+          key={`liked-post-by-${username}}`}
+          posts={likedPosts}
+          showUser={true}
+        />
+      </ul>
+    </div>
+  );
+};
 
-export default LikedPosts
+export default LikedPosts;
