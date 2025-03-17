ca8208387011b37f2fa52d67d3ff32fd08750941
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-17 15:01:53 -0500

    refactor: clean up AuthContext for improved readability and consistency

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,95 +1,92 @@
-import { getCurrentUser } from '@/lib/appwrite/api'
-import { IContextType, IUser } from '@/types'
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IContextType, IUser } from "@/types";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 export const INITIAL_USER = {
-    id: '',
-    name: '',
-    username: '',
-    email: '',
-    imageId: '',
-    imageUrl: '',
-    bio: ''
-}
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageId: "",
+  imageUrl: "",
+  bio: "",
+};
 
 const INITIAL_STATE: IContextType = {
-    user: INITIAL_USER,
-    isLoading: false,
-    isAuthenticated: false,
-    setUser: () => { },
-    setIsAuthenticated: () => { },
-    checkAuthUser: async () => false as boolean
-}
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
 
-const AuthContext = createContext<IContextType>(INITIAL_STATE)
+const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [user, setUser] = useState<IUser>(INITIAL_USER)
-    const [isLoading, setIsLoading] = useState<boolean | null>(null)
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null)
-    // const [isLoading, setIsLoading] = useState(false)
-    // const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const navigate = useNavigate()
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState<boolean | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const navigate = useNavigate();
 
-    /**
-      * Asynchronously checks if the user is authenticated by retrieving the current user account.
-      * If the account exists, it updates the user state with the account information and sets the
-      * authenticated state to true. Returns true if the account exists, false otherwise.
-      *
-      * @return {Promise<boolean>} A promise that resolves to true if the account exists, false otherwise.
-      */
-    const checkAuthUser = async () => {
-        try {
-            setIsLoading(true)
-            const currentAccount = await getCurrentUser()
-            console.log(currentAccount)
-            if (currentAccount) {
-                setUser({
-                    id: currentAccount.$id,
-                    name: currentAccount.name,
-                    username: currentAccount.username,
-                    email: currentAccount.email,
-                    imageId: currentAccount.imageId,
-                    imageUrl: currentAccount.imageUrl,
-                    bio: currentAccount.bio
-                })
-                setIsAuthenticated(true)
-                return true
-            }
-            setIsAuthenticated(false)
-            return false
-        } catch (error) {
-            setIsAuthenticated(false)
-            console.error(error)
-            return false
-        } finally {
+  /**
+   * Asynchronously checks if the user is authenticated by retrieving the current user account.
+   * If the account exists, it updates the user state with the account information and sets the
+   * authenticated state to true. Returns true if the account exists, false otherwise.
+   *
+   * @return {Promise<boolean>} A promise that resolves to true if the account exists, false otherwise.
+   */
+  const checkAuthUser = async () => {
+    try {
+      setIsLoading(true);
+      const currentAccount = await getCurrentUser();
 
-            setIsLoading(false)
-        }
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageId: currentAccount.imageId,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+        setIsAuthenticated(true);
+        return true;
+      }
+      setIsAuthenticated(false);
+      return false;
+    } catch (error) {
+      setIsAuthenticated(false);
+
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      localStorage.getItem("cookieFallback") === "[]"
+      // localStorage.getItem('cookieFallback') === null
+    ) {
+      navigate("/sign-in");
     }
 
-    useEffect(() => {
-        if (
-            localStorage.getItem('cookieFallback') === '[]'
-            // localStorage.getItem('cookieFallback') === null
-        ) { navigate('/sign-in') }
-
-        checkAuthUser()
-    }, [])
-    const value: IContextType = {
-        user,
-        isLoading,
-        isAuthenticated,
-        setUser,
-        setIsAuthenticated,
-        checkAuthUser
-    }
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
+    checkAuthUser();
+  }, []);
+  const value: IContextType = {
+    user,
+    isLoading,
+    isAuthenticated,
+    setUser,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
-export default AuthProvider
-export const useUserContext = () => useContext(AuthContext)
+export default AuthProvider;
+export const useUserContext = () => useContext(AuthContext);
