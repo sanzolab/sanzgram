3f189eb961712a5f034721f034f29b51f96b7484
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-18 17:43:49 -0500

    refactor: simplify AuthLayout component by utilizing useUserContext for authentication state

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,23 +1,27 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { useUserContext } from "@/context/AuthContext";
+import { Outlet, Navigate } from "react-router-dom";
 
 function AuthLayout() {
-    const isAuthenticated = false
-    return (
+  const { isAuthenticated } = useUserContext();
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
         <>
-            {
-                isAuthenticated ? (
-                    <Navigate to='/' />
-                ) : (
-                    <>
-                        <section className='flex flex-1 justify-center items-center flex-col'>
-                            <Outlet />
-                        </section>
-                        <img className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' src='/public/assets/images/side-img.svg' alt='logo' />
-                    </>
-                )
-            }
+          <section className="flex flex-1 justify-center items-center flex-col">
+            <Outlet />
+          </section>
+
+          <img
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            src="assets/images/side-img.svg"
+            alt="logo"
+          />
         </>
-    )
+      )}
+    </>
+  );
 }
 
-export default AuthLayout
+export default AuthLayout;
