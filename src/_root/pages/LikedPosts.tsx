410b4bca5384a742bc504cec18b7c810441417b8
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-19 14:44:13 -0500

    refactor: add empty state handling for liked posts in LikedPosts component

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,20 +1,35 @@
 import GridPostList from "@/components/shared/GridPostList";
 import { Models } from "appwrite";
+import { Link } from "react-router-dom";
 
 type LikedPostsProps = {
   likedPosts: Models.Document;
   username: string;
 };
 const LikedPosts = ({ likedPosts, username }: LikedPostsProps) => {
+  const isLikedPostsEmpty = likedPosts.length === 0;
   return (
     <div className="w-full mt-12 relative">
-      <ul className="grid grid-cols-auto-fit">
-        <GridPostList
-          key={`liked-post-by-${username}}`}
-          posts={likedPosts}
-          showUser={true}
-        />
-      </ul>
+      {isLikedPostsEmpty ? (
+        <div className="flex items-center flex-col gap-4">
+          <p className="italic">No Liked Post</p>
+          <Link
+            className="shad-button_primary group mt-2 py-2 px-4  h-fit w-fit rounded-md
+                  "
+            to={"/explore"}
+          >
+            <p className="subtle-semibold">Explore and Like your First Post</p>
+          </Link>
+        </div>
+      ) : (
+        <ul className="grid grid-cols-auto-fit">
+          <GridPostList
+            key={`liked-post-by-${username}}`}
+            posts={likedPosts}
+            showUser={true}
+          />
+        </ul>
+      )}
     </div>
   );
 };
