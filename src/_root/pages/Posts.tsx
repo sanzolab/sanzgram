0992ba3f9b362e9e952450efb7a6cda7bc02a4a1
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-19 15:54:29 -0500

    refactor: add current user check for creating posts in Posts component

--- src/_root/pages/Posts.tsx
+++ src/_root/pages/Posts.tsx
@@ -5,23 +5,25 @@ import { Link } from "react-router-dom";
 type PostsProps = {
   posts: Models.Document;
   username: string;
+  isCurrentUser: boolean;
 };
-const Posts = ({ posts, username }: PostsProps) => {
+const Posts = ({ posts, username, isCurrentUser }: PostsProps) => {
   const isPostsEmpty = posts?.length === 0;
 
-  console.log(posts);
   return (
     <div className="w-full mt-12">
       {isPostsEmpty ? (
         <div className="flex items-center flex-col gap-4">
           <p className="italic">No Posts Created</p>
-          <Link
-            className="shad-button_primary group mt-2 py-2 px-4  h-fit w-fit rounded-md
+          {isCurrentUser && (
+            <Link
+              className="shad-button_primary group mt-2 py-2 px-4  h-fit w-fit rounded-md
             "
-            to={"/create-post"}
-          >
-            <p className="subtle-semibold">Create your First Post</p>
-          </Link>
+              to={"/create-post"}
+            >
+              <p className="subtle-semibold">Create your First Post</p>
+            </Link>
+          )}
         </div>
       ) : (
         <ul className="grid grid-cols-auto-fit">
