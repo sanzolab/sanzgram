7fabbc41acc362d224b61c762874c8b3b7ea0e9d
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-19 12:31:21 -0500

    refactor: update image source paths in TopBar component

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,36 +1,47 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from '../ui/button'
-import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
-import { useEffect } from 'react'
-import { useUserContext } from '@/context/AuthContext'
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
 
 const TopBar = () => {
-    const { mutate: signOut, isSuccess } = useSignOutAccount()
-    const navigate = useNavigate()
-    const { user } = useUserContext()
-    useEffect(() => {
-        if (isSuccess) navigate(0)
-    }, [isSuccess])
-    return (
-        <section className='topbar'>
-            <div className='flex-between py-4 px-5'>
-            <Link to='/' className='flex items-center gap-2'>
-                    <img src='/public/assets/images/sanzgramlogo3-01.svg' alt="logo"height={32}  width={115
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+  return (
+    <section className="topbar">
+      <div className="flex-between py-4 px-5">
+        <Link to="/" className="flex items-center gap-2">
+          <img
+            src="/assets/images/sanzgramlogo3-01.svg"
+            alt="logo"
+            height={32}
+            width={115}
+          />
+        </Link>
 
-                    } />
-                </Link>
+        <div className="flex gap-4">
+          <Button
+            variant={"ghost"}
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+          <Link className="flex-center gap-3" to={`/profile/${user.id}`}>
+            <img
+              className="h-8 w-8 rounded-full"
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt=""
+            />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-                <div className='flex gap-4'>
-                    <Button variant={'ghost'} className='shad-button_ghost' onClick={() => signOut()}>
-                        <img src="/public/assets/icons/logout.svg" alt="logout" />
-                    </Button>
-                    <Link className='flex-center gap-3' to={`/profile/${user.id}`}>
-                        <img className='h-8 w-8 rounded-full' src={user.imageUrl || '/public/assets/icons/profile-placeholder.svg'} alt="" />
-                    </Link>
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default TopBar
+export default TopBar;
