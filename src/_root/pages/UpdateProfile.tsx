a731af2cdeec8554286a4b9c7adf60bbab69008f
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2025-03-20 15:49:14 -0500

    refactor: remove unnecessary whitespace in UpdateProfile component

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -15,7 +15,6 @@ const UpdateProfile = () => {
   } = useUserContext();
 
   const [loadingInitial, setLoadingInitial] = useState(true);
-
   useEffect(() => {
     if (isUserLoading === null || isUserAuthenticated === null) {
       return;
